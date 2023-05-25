c0f9e5dec4f79afff18415f08f0e110bb5e76580
Tuğsem Saranlı <saranlitugsem@gmail.com>
2023-05-25 18:59:03 +0300

    Add devise for authentication and rspec gem for testing

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module ECommerceApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.generators.test_framework = :rspec
     config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  # resources :users
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#index"
 end
