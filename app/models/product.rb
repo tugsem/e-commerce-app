5f62b084b6422caa94215163ad444e5804c3e69b
Tuğsem Saranlı <saranlitugsem@gmail.com>
2023-06-01 15:15:47 +0300

    Add validations for user and product models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ApplicationRecord
+  validates :product_name, presence: true
+  validates :product_quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :description, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :product_image, presence: true
 end
