188b9bcb8f941f05743f80ccc6d9a240179f0a3b
Tuğsem Saranlı <saranlitugsem@gmail.com>
2023-06-01 15:16:31 +0300

    Add bootstrap form to the forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
