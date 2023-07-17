b84d8a0ec700967d85d5c915e41bdea38d9dabb8
Tuğsem Saranlı <saranlitugsem@gmail.com>
2023-07-17 23:50:22 +0300

    Add turbo stream for editing cart items' quantity and price

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,23 +1,34 @@
 class CartController < ApplicationController
   def show; end
 
-  def add
+  def create
     @product = Product.find(params[:id])
     quantity = params[:quantity].to_i
     current_orderable = @cart.orderables.find_by(product_id: @product.id)
-    if current_orderable && quantity.positive?
-      current_orderable.update(quantity:)
-      turbo_stream.append(render_to_string(partial: 'orderables', locals: { orderable: current_orderable }))
-    elsif quantity <= 0
-      current_orderable.destroy
+    if current_orderable
+      current_orderable.update(quantity: quantity)
+      #turbo_stream.append(render_to_string(partial: 'orderables', locals: { orderable: current_orderable }))
     else
-      @cart.orderables.create(product: @product, quantity:)
-      redirect_to products_path, notice: 'Item added to cart.'
+      @cart.orderables.create(product: @product, quantity: quantity)
+      turbo_stream
     end
   end
 
-  def remove
+  def update
+    @current_orderable = @cart.orderables.find_by(product_id: params[:id])
+      if @current_orderable
+        quantity = params[:quantity].to_i
+        @current_orderable.update(quantity: quantity)
+        @orderable_id = @current_orderable.id
+        puts "-----------------------------" + @current_orderable.total.to_s + "-----------------------------"
+        turbo_stream
+      else
+        puts "Couldn't find orderable"
+      end
+  end
+
+  def destroy
     Orderable.find_by(id: params[:id]).destroy
-    redirect_to cart_path, notice: 'Item successfully removed.'
+    turbo_stream
   end
 end
