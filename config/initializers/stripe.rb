09b772aee14bc38474792d23cc0cc723e0fa42f8
Tuğsem Saranlı <saranlitugsem@gmail.com>
2023-08-09 15:50:29 +0300

    add stripe configuration

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,1 +1,6 @@
-Stripe.api_key = Rails.application.credentials.stripe_secret_key
+Rails.configuration.stripe = {
+  :publishable_key => ENV['pk_test_KEY'],
+  :secret_key      => ENV['sk_test_KEY']
+}
+
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
