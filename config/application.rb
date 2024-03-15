fb31d2fd108a82112b186e84bced4bc695d55170
Tuğsem Saranlı <saranlitugsem@gmail.com>
2024-03-15 17:18:53 +0300

    change host

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module ECommerceApp
     config.generators.test_framework = :rspec
     config.load_defaults 7.0
     # config/application.rb
-    config.action_mailer.default_url_options = { host: "http://localhost:3000" }
+    config.action_mailer.default_url_options = { host: "http://0.0.0.0:3000" }
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
