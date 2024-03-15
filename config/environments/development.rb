0f00a2fb50a62710150b8945b9147f2858772aa2
Tuğsem Saranlı <saranlitugsem@gmail.com>
2024-03-15 17:04:21 +0300

    change host to 0.0.0.0

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -72,5 +72,5 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: '0.0.0.0', port: 3000 }
 end
